6616e264ff31e59af52c4c2543efcadc9c486466
git-yashk <yashk.n6+github@gmail.com>
2024-10-24 00:54:01 +0530

    added responsive styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <header className="h-10 border-b-4 border-[#09090b] flex justify-between items-center px-8">
             <div className="flex gap-2 select-none items-center">
                 <CodeIcon height="20" />
-                <span>Code.render</span>
+                <span className="font-bold">Code.render</span>
             </div>
             <div>
                 <a href="https://github.com/git-yashk/coderender" target="_blank"><GithubIcon /></a>
