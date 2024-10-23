6f7e953235a811624a7ad98e2e5d7f848860a9b8
git-yashk <yashk.n6+github@gmail.com>
2024-10-23 16:16:24 +0530

    configured appwrite

---
+++ src/appwrite/databases.js
@@ -0,0 +1,40 @@
+import { Databases, ID } from "appwrite";
+import { client } from "./config";
+import { appwrite } from "../env";
+
+const databases = new Databases(client);
+
+const db = {};
+
+db["shareCode"] = {
+
+    createDocument: async (payload) => {
+        try {
+            const response = await databases.createDocument(
+                appwrite.databaseID,
+                appwrite.collectionID,
+                ID.unique(),
+                payload
+            );
+            return response;
+        } catch (error) {
+            console.error("Error creating document:", error);
+            return null;
+        }
+    },
+
+    getDocument: async (documentID) => {
+        try {
+            const response = await databases.getDocument(
+                appwrite.databaseID,
+                appwrite.collectionID,
+                documentID
+            );
+            return response;
+        } catch (error) {
+            console.error("Error getting document:", error);
+            return null;
+        }
+    },
+
+}
